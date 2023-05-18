5dfb8efe4af7823a6adda2fac6717e86f4fa521e
VictorOlegario <126923917+VictorOlegario@users.noreply.github.com>
2023-05-17 22:16:52 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Aula Lab Teste"
+    return "Victor _ tia 31855016"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
